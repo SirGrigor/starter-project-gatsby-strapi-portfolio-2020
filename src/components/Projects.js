03a2a65119568b4d14b7b9fb50b15abd947c7281
grigorjev.ilja@gmail.com <werbatim58803110>
2020-11-26 09:39:42 +0200

    Project components realisation in projects feature

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,8 +2,21 @@ import React from "react"
 import Title from "./Title"
 import Project from "./Project"
 import { Link } from "gatsby"
-const Projects = () => {
-  return <h2>projects list</h2>
+import index from "../pages"
+const Projects = ({projects, title, showLink}) => {
+  return <section className='section project'>
+    <Title title={title}/>
+    <div className='services-center project-center'>
+      {projects.map((project,index)=>{
+        return <Project key={project.id} index={index} {...project}/>
+      })}
+    </div>
+    {
+      showLink && (<Link to="/project" className='btn center-btn'>
+        projects
+      </Link>)
+    }
+  </section>
 }
 
 export default Projects
