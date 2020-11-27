8540962c809e04e32d993e6d56668a7484835d82
grigorjev.ilja@gmail.com <werbatim58803110>
2020-11-27 20:01:49 +0200

    Final build and Deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,19 +37,5 @@ module.exports = {
         singleTypes: ['about'],
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-prefetch-google-fonts`,
-    //   options: {
-    //     fonts: [
-    //       {
-    //         family: `Roboto`,
-    //         variants: [`400`, `700`],
-    //       },
-    //       {
-    //         family: `Open Sans`,
-    //       },
-    //     ],
-    //   },
-    // },
   ],
 }
